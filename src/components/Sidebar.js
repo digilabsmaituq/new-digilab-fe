25ccb3c95ef768aeeddfe73ecc8b2e3b20be1251
Rayhan Rusyd <antianobl@Gmail.com>
2022-07-05 11:29:09 +0700

    first commit to review

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import "./Sidebar.css"
 import { useState } from "react"
+import { UilEstate, UilUsersAlt, UilSignin } from '@iconscout/react-unicons'
 
 function Sidebar(){
     const [isNavExpanded, setNavExpanded] = useState(false)
@@ -10,17 +11,17 @@ function Sidebar(){
                     <h1><button onClick={()=> {setNavExpanded(!isNavExpanded)}}><i class="bi bi-list"></i></button> Digilab</h1>
                 </div>
                 <div className={ isNavExpanded ? "navbar-expand expand-nav" : "navbar-expand"}>
-                    <li className="nav-item-expand"><a href="#">Home</a></li>
-                    <li className="nav-item-expand"><a href="#">Organizer</a></li>
-                    <li className="nav-item-expand"><a href="#">Recruitment</a></li>
+                    <li className="nav-item-expand"><a href="#"><UilEstate /> Home</a></li>
+                    <li className="nav-item-expand"><a href="#"><UilUsersAlt /> Organizer</a></li>
+                    <li className="nav-item-expand"><a href="#"><UilSignin /> Recruitment</a></li>
                 </div>
             </div>
             <div className="sidebar">
-                <ul>
-                    <li className="nav-item"><a href="#">Home</a></li>
-                    <li className="nav-item"><a href="#">Organizer</a></li>
-                    <li className="nav-item"><a href="#">Recruitment</a></li>
-                </ul>
+                <div className="nav">
+                    <a className="nav-item" href="#"><UilEstate /> Home</a>
+                    <a className="nav-item" href="#"><UilUsersAlt /> Organizer</a>
+                    <a className="nav-item" href="#"><UilSignin /> Join Us</a>
+                </div>
                 <div className="social">
                     <h1>Digilab SMAITUQ</h1>
                     <a href="#"><i class="bi bi-instagram"></i></a> <a href="#"><i class="bi bi-github"></i></a>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,21 +2,37 @@ import "./Home.css"
 import banner from "../images/stock1.jpg"
 import vision from "../images/magnifying-glass.png"
 import galeri from "../data/activity.json"
+import { UilAirplay } from '@iconscout/react-unicons'
 
 function Home(){
     return(
         <div className="home">
             <section id="About">
                 <img className="banner" src={ banner } />
-                <h1>What is Digilab?</h1>
+                <h1>What is <span>Digilab</span>?</h1>
                 <p>DigiLab is an IT community based on Ummul Quro Bogor Islamic High School. This is a place where students from Ummul Quro High School could improve or learn new IT skills.</p>
                 <div className="row">
                     <div className="col">
                         <img className="vision-img" src={ vision } />
                     </div>
                     <div className="col">
-                        <h1>Our Vision</h1>
-                        <p>Our vision is to provide a place for Ummul Quro Islamic High School students who want to improve their IT skills.</p>
+                        <div className="vision">
+                            <h1>Our Vision</h1>
+                            <p>Our vision is to provide a place for Ummul Quro Islamic High School students who want to improve their IT skills.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="Team">
+                <h1>The Team</h1>
+                <div className="row">
+                    <div className="col">
+                        <div className="team-card">
+                            
+                        </div>
+                    </div>
+                    <div className="col">
+
                     </div>
                 </div>
             </section>
